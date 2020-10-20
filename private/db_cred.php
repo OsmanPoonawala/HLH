3255fbcf49338b17a003603e754c289e2bb3af37
OsmanPoonawala <45496552+OsmanPoonawala@users.noreply.github.com>
2020-10-21 01:08:57 +0500

    Update db_cred.php

--- private/db_cred.php
+++ private/db_cred.php
@@ -1,8 +1,8 @@
 <?php
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "root");
-define("DB_NAME", "HLHRadiology");
+define("DB_SERVER", "ryfqldzbliwmq6g5.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
+define("DB_USER", "b26ptr5zye1lgex4");
+define("DB_PASS", "la4rd1bj7f6yohrb");
+define("DB_NAME", "t305eflgzk8uefv6");
 
 ?>
